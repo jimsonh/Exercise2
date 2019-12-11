2532cbcc95ce41cc8e3e58e3e65fced0d376f8f3
jimsonh <jimsonho2010@hotmail.com>
2019-12-12 05:10:55 +0900

    update

--- app/src/main/java/com/example/exercise2/MainActivity.kt
+++ app/src/main/java/com/example/exercise2/MainActivity.kt
@@ -2,54 +2,72 @@ package com.example.exercise2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.exercise2.databinding.ActivityMainBinding
 import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var weight:EditText
-    lateinit var height:EditText
-    lateinit var bmi:TextView
-    lateinit var image:ImageView
+    private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        weight.findViewById<EditText>(R.id.editTextWeight)
-        height.findViewById<EditText>(R.id.editTextHeight)
-        bmi.findViewById<TextView>(R.id.textViewBMI)
-        image.findViewById<ImageView>(R.id.imageViewProfile)
-        image.setImageResource(R.drawable.empty)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val calButton = findViewById<Button>(R.id.buttonCalculate)
-        val resetButton = findViewById<Button>(R.id.buttonReset)
-        calButton.setOnClickListener{cal()}
-        resetButton.setOnClickListener{reset()}
+        binding.buttonCalculate.setOnClickListener{
+            cal(it)
+        }
+
+        binding.buttonReset.setOnClickListener{
+            reset()
+        }
+
+
+
+
+
 
     }
 
-    private fun cal(){
-        val bmiDouble = weight.toString().toDouble()/(height.toString().toDouble()/100).pow(2)
-        bmi.text="BMI: "+bmiDouble
+    private fun cal(view: View){
+        binding.apply {
 
-        if(bmiDouble<18.5)
-            image.setImageResource(R.drawable.under)
-        else if(bmiDouble<25)
-            image.setImageResource(R.drawable.normal)
-        else
-            image.setImageResource(R.drawable.over)
+
+            val height = editTextHeight.text.toString().toDouble()
+            val weight = editTextWeight.text.toString().toDouble()
+            val bmi = weight/((height/100).pow(2))
+            textViewBMI.text= "BMI: %.2f ".format(bmi)
+
+            when {
+                bmi<18.5 -> imageViewProfile.setImageResource(R.drawable.under)
+                bmi<25 -> imageViewProfile.setImageResource(R.drawable.normal)
+                else -> imageViewProfile.setImageResource(R.drawable.over)
+            }
+
+
+        }
+
+
 
     }
 
     private fun reset(){
-        image.setImageResource(R.drawable.empty)
-        weight.setText(R.string.weight)
-        height.setText(R.string.height)
-        bmi.setText(R.string.bmi)
+        binding.apply {
+            imageViewProfile.setImageResource(R.drawable.empty)
+            editTextWeight.setText(R.string.weight)
+            editTextHeight.setText(R.string.height)
+            textViewBMI.setText(R.string.bmi)
+
+        }
+
+
 
     }
 }
